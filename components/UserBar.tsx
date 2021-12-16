8871c86f7a7a6f4cd42c955753e0e6d3f52bf868
Jeremy Muñoz Torres <JMunoz@contractor.nexstardigital.com>
2021-12-15 20:13:10 -0400

    Added search bar for creators

--- components/UserBar.tsx
+++ components/UserBar.tsx
@@ -1,5 +1,7 @@
 import { NextPage } from 'next';
+import { ChangeEvent, useState } from 'react';
 import { PaginationResponse, User } from '../types';
+import debounce from 'lodash.debounce';
 
 import UserCard from './UserCard';
 
@@ -9,11 +11,29 @@ interface T {
   users: PaginationResponse<User>;
 }
 
-const UserBar: NextPage<T> = ({
-  users,
-  selected,
-  className = '',
-}) => {
+const UserBar: NextPage<T> = ({ users, selected, className = '' }) => {
+  const [renderedUsers, setRenderedUsers] = useState(users);
+  const [notFound, setNotFound] = useState(false);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
+
+    fetch(`http://127.0.0.1:8000/users/?q=${query}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.count) {
+          setRenderedUsers(data);
+          setNotFound(false);
+        } else {
+          setRenderedUsers(users);
+          setNotFound(true);
+        }
+      })
+      .catch((e) => setRenderedUsers(users));
+  };
+
+  const debouncedChangeHandler = debounce(handleChange, 300);
+
   return (
     <nav className={className}>
       <header className="p-4 pb-0 sticky top-0 bg-white z-10 shadow-lg">
@@ -26,13 +46,18 @@ const UserBar: NextPage<T> = ({
 
         <input
           className="bg-gray-100 my-4 p-1 rounded w-full"
-          placeholder="Search top creator..."
+          onChange={debouncedChangeHandler}
+          placeholder="🔎 Search top creator..."
           type="text"
         />
       </header>
 
       <section>
-        {users.results.map((user) => (
+        {notFound && (
+          <p className="text-center">No creators under your search ☹️</p>
+        )}
+
+        {renderedUsers.results.map((user) => (
           <UserCard
             isSelected={user.id === selected?.id}
             key={user.id}
